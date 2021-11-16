2f98181a047b120586a0a1a0653c2736bc626a09
Liangwh <13660161032@163.com>
2021-11-16 17:40:56 +0800

    Rest client

---
+++ rest-client/src/main/java/xyz/lwh/spring/cloud/restclient/demo/DemoConfig.java
@@ -0,0 +1,18 @@
+package xyz.lwh.spring.cloud.restclient.demo;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class DemoConfig {
+
+    @LoadBalanced
+    @Bean("demoRestTemplate")
+    public RestTemplate createDemoRestTemp(){
+        return new RestTemplate();
+    }
+}
+
