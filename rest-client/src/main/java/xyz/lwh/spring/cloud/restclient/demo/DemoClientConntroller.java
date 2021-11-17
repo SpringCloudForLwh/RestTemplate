21f0a1d210b9e141532c8a9329c1274bbef84c6f
Liangwh <13660161032@163.com>
2021-11-17 11:02:09 +0800

    Rest postForLocation

--- rest-client/src/main/java/xyz/lwh/spring/cloud/restclient/demo/DemoClientConntroller.java
+++ rest-client/src/main/java/xyz/lwh/spring/cloud/restclient/demo/DemoClientConntroller.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("demo")
 public class DemoClientConntroller {
@@ -14,10 +19,23 @@ public class DemoClientConntroller {
     @Autowired
     @Qualifier("demoRestTemplate")
     RestTemplate restTemplate;
-@RequestMapping("map")
+    @RequestMapping("map")
     public String map(){
         String forObject = restTemplate.getForObject("http://REST-SERVER/demo/1/map", String.class);
         return forObject;
+    }
+
+    /**
+     * postForLocation也是提交新资源，提交成功之后，返回新资源的URI，
+     * postForLocation的参数和前面两种的参数基本一致，只不过该方法的返回值为Uri，这个只需要服务提供者返回一个Uri即可，该Uri表示新资源的位置。
+     * @return
+     */
+    @RequestMapping("location")
+    public URI location(){
+        Map<String,String> map = Collections.singletonMap("myName","liangwh");
+        URI locatipn = restTemplate.postForLocation("http://REST-SERVER/demo/1/location",map,Map.class);
+        return locatipn;
+
     }
 
 }
--- rest-service/src/main/java/xyz/lwh/spring/cloud/restservice/demo/Demo01.java
+++ rest-service/src/main/java/xyz/lwh/spring/cloud/restservice/demo/Demo01.java
@@ -1,8 +1,15 @@
 package xyz.lwh.spring.cloud.restservice.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("demo/1")
@@ -15,4 +22,14 @@ public class Demo01 {
     public String map(){
         return var;
     }
+
+    @RequestMapping("location")
+    public URI location(@RequestBody Map<String,String> body, HttpServletResponse response, HttpServletRequest request) throws URISyntaxException {
+
+        URI uri = new URI("https://www.baidu.com/s?wd="+body.get("myName"));
+        response.addHeader("Location",uri.toString());
+
+        return uri;
+    }
+
 }
